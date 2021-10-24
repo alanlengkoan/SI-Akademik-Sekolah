b6fdee7e1ce00cd349613b5c66cca901a30c99f0
Alan Lengkoan <alanlengkoan15@gmail.com>
2021-10-24 10:35:58 +0800

    update css

--- application/views/home/s_alumni/css/view.php
+++ application/views/home/s_alumni/css/view.php
@@ -1,3 +1,1 @@
-  <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>admin/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>admin/pages/data-table/css/buttons.dataTables.min.css">
-  <link rel="stylesheet" type="text/css" href="<?= assets_url() ?>admin/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css">
