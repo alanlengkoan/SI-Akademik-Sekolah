d778b0c540232873d67dae70e3afbf2125f11cfb
Alan Lengkoan <alanlengkoan15@gmail.com>
2021-10-24 10:46:13 +0800

    update

--- application/views/home/tentang/view.php
+++ application/views/home/tentang/view.php
@@ -1,6 +1,6 @@
 <!-- start banner Area -->
-<section class="banner-area relative about-banner" id="home">
-    <div class="overlay overlay-bg"></div>
+<section class="about-banner relative" id="home">
+    <div class="overlay"></div>
     <div class="container">
         <div class="row d-flex align-items-center justify-content-center">
             <div class="about-content col-lg-12">
